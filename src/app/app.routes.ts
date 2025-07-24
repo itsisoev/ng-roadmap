28ebe23b4cd9488369e3b962351cd54cd61a8731
Daler <isoev9733@gmail.com>
2025-07-24 19:35:40 +0500

    Скрываем наш header при переходе login, register. Создал компонент login, register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'auth',
+    loadChildren: () => import('./features/auth/auth.routes').then(m => m.authRoutes),
+  },
+  {
+    path: '',
+    children: [],
+  }
+];
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,8 +1,11 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {RouterLink} from '@angular/router';
 
 @Component({
   selector: 'layout-header',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './header.html',
   styleUrl: './header.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
